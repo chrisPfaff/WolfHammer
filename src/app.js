640a61fb2162cc2ba31bd3c7bc743b2944ee32cd
Greg Pfaff <greg.pfaff@razorfish.com>
2018-02-15 16:28:47 -0600

    adding updates to wolf

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import wolf from './js/wolf.js';
-import checkbox from './js/checkbox.js'
-import wolfImage from './img/wolf.jpg'
-import './scss/variables.scss'
-import './scss/mixins.scss'
-import './scss/components/players.scss'
-import './scss/base.scss'
+import checkbox from './js/checkbox.js';
+import wolfImage from '././img/wolf.jpg';
+import saddam from '././img/saddam.jpg';
+import './scss/variables.scss';
+import './scss/mixins.scss';
+import './scss/components/players.scss';
+import './scss/base.scss';
 
 if (module.hot) {
   module.hot.accept();
