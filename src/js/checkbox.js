cb58de052f714aec0794b92a1301db1348b2f99d
Greg Pfaff <greg.pfaff@razorfish.com>
2018-02-15 21:46:17 -0600

    wip checkboxes

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -1,15 +1,33 @@
 const submit = document.querySelector(".submit");
+const set_teams = document.querySelector('.set_teams');
 
 let array = [];
 
 document.addEventListener("click", event => {
   if (!array.includes(event.target) && event.target.matches("input")) {
-    array.push(event.target);
+    countCheckboxes(event.target, set_teams);
+    console.log(typeof event.target);
+    if (!event.target.checked) {
+      set_teams.removeChild(event.target.lastChild);
+    }
+    // if (array.length >= 3) {
+    //   return;
+    // }
   } else {
     removeVal(array, event.target);
   }
   return array;
 });
+
+function countCheckboxes(check_value, parentElement) {
+  const newElem = document.createElement('p');
+  const name = document.createTextNode(check_value.name);
+  // name = JSON.stringify(name);
+  newElem.classList.add('teamz');
+
+  newElem.appendChild(name);
+  parentElement.appendChild(newElem);
+}
 
 function removeVal(arr, val) {
   for (var i = 0; i < arr.length; i++) {
