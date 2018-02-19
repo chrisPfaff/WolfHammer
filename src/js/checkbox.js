7ec8288ea6b31a4fdff4dc835c3ce679b073e11e
Greg Pfaff <greg.pfaff@razorfish.com>
2018-02-18 21:22:05 -0600

    adding data changes

--- src/js/checkbox.js
+++ src/js/checkbox.js
@@ -1,20 +1,15 @@
-const submit = document.querySelector(".submit");
+const createTeam = document.querySelector('.create');
 const setTeams = document.querySelector('.set_teams');
 const checkboxes = document.querySelectorAll('input');
 const teamz = setTeams.querySelectorAll('.teamz');
+const scoreBoard = document.querySelector('.score');
+const dots = Array.from(document.querySelectorAll('.dots'));
 
 let teams = [];
+let score = 0;
 
-document.addEventListener("click", event => {
-  if (!teams.includes(event.target) && event.target.matches("input")) {
-    createTeams(event.target, setTeams);
-  } else {
-    removeTeam(checkboxes, event.target);
-  }
-  return teams;
-});
 
-function createTeams(checkValue, parentElement) {
+const addTeamsToDOM = (checkValue, parentElement) => {
   if (checkValue.checked) {
     var newElem = document.createElement('p');
     var name = document.createTextNode(checkValue.name);
@@ -29,20 +24,14 @@ function createTeams(checkValue, parentElement) {
   }
 }
 
-function removeTeam(arr, val) {
+const removeTeamFromDOM = (arr, val) => {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] == val) arr.splice(i, 1);
   }
 }
 
-function settingTeams(arr, target) {
-  const players = Array.from(document.querySelectorAll('input:checked')).map(input => {
-    return {
-      id: input.id,
-      name: input.name,
-      score: 0
-    };
-  });
+const createTeams = (arr, target) => {
+  const players = Array.from(document.querySelectorAll('input:checked')).map(input => { return { id: input.id, name: input.name }; });
 
   if (players.length) {
     teams.push({
@@ -50,74 +39,26 @@ function settingTeams(arr, target) {
       score: 0,
       id: teams.length
     })
-  } else {
-      console.log('delete', target.id);
-      removeTeam(arr, target);
+    console.log('players', players);
+  }
+  if (!players.length) {
+    console.error('PICK A PLAYER');
   }
 }
 
-
-submit.addEventListener("click", (event) => {
-  const paused = submit.classList.toggle('paused');
-  event.preventDefault();
-  settingTeams(checkboxes, event.target.id)
-
-  if (paused) {
-    submit.textContent = 'edit teams';
-    checkboxes.forEach(checkbox => checkbox.setAttribute('disabled', true));
-  } else {
-    submit.textContent = 'Set Teams';
-    checkboxes.forEach(checkbox => checkbox.removeAttribute('disabled'));
-  }
-
-  // console.log(setTeams);
-  // const newArray = Array.from(array);
-  // let total = 0;
-
-  // newArray.map((item, i) => {
-  //   total += Number(item.value);
-  //   if (newArray.length === 3) {
-  //     console.log('lone wolf', total)
-  //     total = total * 3;
-  //     return total;
-  //   } else {
-  //     console.log(total);
-  //     return total;
-  //   }
-  // })
-});
-
-const scoreBoard = document.querySelector('.score');
-const dots = Array.from(document.querySelectorAll('.dots'));
-
-let score = 0;
-
-function calculateScore(event, target) {
+const calculateScore = (event, target) => {
   event.preventDefault();
   const current = event.target;
   if (teams.length) {
 
     const activeTeamID = teams.filter(team => team.players[0]);
 
-    // const {name, id} = activeTeamID.players;
-    // const { score } = activeTeamID;
-
-    // console.log(name, score, id);
-
-   // const activeTeamId = teams.filter(team => team.players[0]);
     const activeTeam = activeTeamID.filter(team => team.id === activeTeamID[0].id)[0];
 
-    //activeTeam.score += `Score: ${score += Number(current.getAttribute('data-dot'))}`
-    // const activeTeamId = parseInt(document.querySelectorAll('#active-team')[0].value);
-    //activeTeam.score += parseInt(current.dataset.dot);
-    //activeTeam.innerHTML = `Score: ${score} += Number(current.getAttribute('data-dot'))}`
     activeTeam.score += parseInt(current.dataset.dot);
     updateTeamData(activeTeam);
   }
 }
-
-dots.forEach(dot => dot.addEventListener('click', calculateScore));
-
 
 const updateTeamData = (activeTeam) => {
   activeTeam.players.forEach((player, index) => {
@@ -125,6 +66,21 @@ const updateTeamData = (activeTeam) => {
   })
 };
 
+// EL's
+document.addEventListener("click", event => {
+  if (!teams.includes(event.target) && event.target.matches("input")) {
+    addTeamsToDOM(event.target, setTeams);
+  } else {
+    removeTeamFromDOM(checkboxes, event.target);
+  }
+  return teams;
+});
+
+createTeam.addEventListener("click", (event) => {
+  createTeams(checkboxes, event.target.id)
+});
+
+dots.forEach(dot => dot.addEventListener('click', calculateScore));
 
 
 // const teams = [];
